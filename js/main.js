9e2d9240a50bd29028c1421aafd2f96f023ce859
Maryam Ali <mali62541@gmail.com>
2015-10-12 04:56:12 -0400

    content

--- js/main.js
+++ js/main.js
@@ -13,11 +13,13 @@
 
 
 // _.each(etsydata.results, function(item){
-//   // console.log('item');
+//   console.log('item');
 //   var itemHTML = templateFunction(item);
 //   $('.searchResults').append(itemHTML);
 // });
 //   console.log('It Works!');
+
+
 
   (function () {
 
@@ -31,7 +33,7 @@ _.each(etsydata.results, function (item) {
  var itemHTML = templateFunction(item);
  var imgUrl = item.Images[0].url_570xN;
  $('.searchResults').append(itemHTML);
- $('.images').append(itemHTML);
+ $('.images').append(imageHTML);
 });
 
 
@@ -40,6 +42,50 @@ _.each(etsydata.results, function (item) {
 }());
 
 
+//   <div class ='searchResults'></div>
+// <div class='images'></div> 
 
+
+//   <script type="text/template" id='itemTemplate'>
+
+//     <div>
+//       <p>
+//         <%=Images[0].url_570xN%>
+//           // <img src="<%=Images[0].url_570xN%>">
+//           <%=title%>
+//       </p>
+//       <p class='price'>
+//         $
+//         <%=price%>
+//       </p>
+//       <p class='description'>
+//         <%=description %>
+//       </p>
+//             <p class='Shop.shop_name'>
+//         <%=shopName %>
+//       </p>
+//       <p class='title'>
+//         <%=title%>
+//       </p>
+//       <p class='link'>
+//         <%=url%>
+//       </p>
+//     </div>
+//     <hr>
+
+
+//   </script>
+
+
+//   <script type="text/template" id="item-template">
+//     <li class="item">
+//       Item Name:
+//       <%=name %>
+//     </li>
+//     <li class='shop_name'>
+//       Shop Name:
+//       <%=shopName %>
+//       </li>
+//     </script>
 
 
